3719a8ea89d53363aedd31749fb817221fe52c50
Usuario <usuario@email.com>
2025-06-19 14:19:09 -0300

    Added commands (falar, somar, embed), events (join, reaction), and scheduled task

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+from datetime import time
 
 load_dotenv()
 
@@ -10,13 +11,64 @@ bot = commands.Bot(".", intents=intents)
 
 @bot.event
 async def on_ready():
-    print("Ola Mundo!!")
+    sincs = await bot.tree.sync()
+    print(f"{len(sincs)} comandos sicronizados")
+    enviar_mensagem.start()
     print("Ola , eu estou online!")
 
-@bot.command()
-async def ola_u(ctx:commands.Context):
+@bot.event
+async def on_message(msg:discord.Message):
     
-    name = ctx.author.name
-    await ctx.reply(f"Olá, {name} eu estou online!!")
+    if msg.author.bot:
+        return
+    await bot.process_commands(msg) 
+    # await msg.reply(f"O usuario {msg.author.mention} mandou uma mensagem no canal {msg.channel.name}")
+
+@bot.event 
+async def on_member_join(membro:discord.Member):
+    canal = bot.get_channel(1348179381524238356)
+    await canal.send (f"{membro.mention} entrou no servidor!!")
+
+@bot.event
+async def on_reaction_add(reacao:discord.Reaction, membro:discord.Member):
+    await reacao.message.reply(f"O membro {membro.mention} reagiu a mensagem com {reacao.emoji}") #usa o membro e a reacao, pega emoji reagido pelo membro
+
+@bot.command()
+async def falar(ctx:commands.Context, *,texto):
+    
+    await ctx.send(texto)
+
+@bot.command()
+async def somar(ctx:commands.Context, n1:int, n2:int):
+
+    resultado = n1 + n2
+
+    await ctx.send(f"A soma entre {n1} e {n2} é {resultado}")
+
+
+
+@bot.command()
+async def enviar_embed(ctx:commands.Context):
+    minha_embed = discord.Embed()
+    minha_embed.title = "Titulo da embed"
+    minha_embed.description = "Descricao da embed"
+
+    imagem = discord.File("img/bomdia.jpg", "bom_dia.jpg")
+    minha_embed.set_image(url="attachment://bom_dia.jpg")
+
+    minha_embed.set_footer(text="Esse e o footer da minha imagem")
+
+    minha_embed.set_author(name="Son Goku", icon_url="https://i.pinimg.com/736x/c2/61/40/c26140a6eba1567aff271cf00eda2726.jpg", url="https://github.com/ruandd9/discord-bot-experiments")
+
+    await ctx.reply(embed=minha_embed, file=imagem)
+
+@tasks.loop(time=time(16, 48))
+async def enviar_mensagem():
+    canal = bot.get_channel(1348179381524238356)
+    await canal.send("Mensagem programada!")
+
+@bot.tree.command()
+async def ola(interact:discord.Interaction):
+    await interact.response.send_message(f"Ola {interact.user.name}!!", ephemeral=True)
 
 bot.run(os.getenv("DISCORD_TOKEN"))
